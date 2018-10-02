fef42c82cc086d74d22d3e7ee6cdb5189271533b
ashleyparrilla <43651597+ashleyparrilla@users.noreply.github.com>
2018-10-02 11:25:45 -0700

    added files from session 2

---
+++ operators.py
@@ -0,0 +1,37 @@
+x = 9
+y = 3
+
+#arithmetic operators 
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y) #modulus (remainders)
+print(x**y) #exponentiation
+x = 9.191823
+print(x//y)  #floor division (drops any decimal places resulting from the division)
+
+#assignment operators
+x = 9   #set x=9
+x += 3  # x=x+3
+print(x)
+x = 9
+x -= 3
+print(x)
+x *= 3  #x=x*3
+print(x)
+x /= 3
+print(x)
+x **= 3
+print(x)
+
+#comparison operators 
+x = 9
+y = 3 
+print(x==y)  #true if x=, false otherwise
+print(x!=y)  #true if x does not =y, false otherwise
+print(x>y)   #true if x>y, false otherwise
+print(x<y)   #true if x<y, false otherwise
+print(x>=y)  #true if x>/=y, "  "
+print(x<=y)  #true if x</=y, "  "
+
